99c2f96437cc4cfa18f13b5457f3b9679a36308e
Seth Bresnett <sgb@softwire.com>
2017-10-31 10:30:02 +0000

    Tests for pawns moving two spaces initially

--- src/test/java/training/chessington/model/pieces/PawnTest.java
+++ src/test/java/training/chessington/model/pieces/PawnTest.java
@@ -40,4 +40,70 @@ public class PawnTest {
         // Assert
         assertThat(moves).contains(new Move(coords, coords.plus(1, 0)));
     }
+
+    @Test
+    public void whitePawnCanMoveUpTwoSquaresIfNotMoved() {
+        // Arrange
+        Board board = Board.empty();
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(6, 4);
+        board.placePiece(coords, pawn);
+
+        // Act
+        List<Move> moves = pawn.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, 0)));
+    }
+
+    @Test
+    public void blackPawnCanMoveDownTwoSquaresIfNotMoved() {
+        // Arrange
+        Board board = Board.empty();
+        Piece pawn = new Pawn(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(1, 4);
+        board.placePiece(coords, pawn);
+
+        // Act
+        List<Move> moves = pawn.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(2, 0)));
+    }
+
+    @Test
+    public void whitePawnCannotMoveUpTwoSquaresIfAlreadyMoved() {
+        // Arrange
+        Board board = Board.empty();
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates initial = new Coordinates(6, 4);
+        board.placePiece(initial, pawn);
+
+        Coordinates moved = initial.plus(-1, 0);
+        board.move(initial, moved);
+
+        // Act
+        List<Move> moves = pawn.getAllowedMoves(moved, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(moved, moved.plus(-2, 0)));
+    }
+
+    @Test
+    public void blackPawnCannotMoveDownTwoSquaresIfAlreadyMoved() {
+        // Arrange
+        Board board = Board.empty();
+        Piece pawn = new Pawn(PlayerColour.BLACK);
+        Coordinates initial = new Coordinates(1, 4);
+        board.placePiece(initial, pawn);
+
+        Coordinates moved = initial.plus(1, 0);
+        board.move(initial, moved);
+
+        // Act
+        List<Move> moves = pawn.getAllowedMoves(moved, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(moved, moved.plus(2, 0)));
+    }
 }
