e047d458b3082da3de1ad701a44034394fc80991
vnguy4 <vanthoai.nguyen@aexp.com>
2021-09-30 11:52:52 +0100

    check that pawn moves up and down

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,6 +15,23 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        // to make a list of move objects
+        List<Move> allowMoves = new ArrayList<>();
+
+        // increment counter direction
+        int increment = 1;  // default is black
+
+        // check colour of pawn and change direction
+        if (this.getColour() == PlayerColour.WHITE) {
+            increment = -1 ;
+        }
+
+        // make the new coordinate object
+        Coordinates to = from.plus(increment, 0);
+
+        // add move object to list of move objects
+        allowMoves.add(new Move(from, to));
+
+        return allowMoves;
     }
 }
