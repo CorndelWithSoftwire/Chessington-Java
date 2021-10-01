433d617f2e348c4d1cefeb256c5635af38347955
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-01 16:15:13 +0100

    king can only move one square at a time in all direction

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -15,6 +15,32 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowableMoves = new ArrayList<>();
+
+        addAvailableSpaces(from, board, allowableMoves);
+
+        return allowableMoves;
+    }
+
+    private void addAvailableSpaces(Coordinates from, Board board, List<Move> initAllowableMoves) {
+        int[] incrementArray = {-1, 0, 1};
+        int fromRow = from.getRow();
+        int fromCol = from.getCol();
+
+        for (int i: incrementArray) {
+            int row = fromRow + i;
+            for (int j: incrementArray) {
+                int col = fromCol + j;
+
+                if (row >= 0 && row <= 7 && col >= 0 && col <= 7) {
+                    Coordinates to = new Coordinates(row, col);
+                    Piece targetPiece = board.get(to);
+                    if (targetPiece == null || targetPiece.getColour() != this.colour) {
+                        initAllowableMoves.add(new Move(from, to));
+                    }
+                }
+
+            }
+        }
     }
 }
--- src/test/java/training/chessington/model/pieces/KingTest.java
+++ src/test/java/training/chessington/model/pieces/KingTest.java
@@ -1,7 +1,36 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class KingTest {
+    @Test
+    public void kingCanMoveOneSquareInAllDirection() {
+        // Arrange
+        Board board = Board.empty();
+        Piece king = new King(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(4, 4);
+        board.placePiece(coords, king);
+
+        // Act
+        List<Move> moves = king.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(0, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(0, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 1)));
+    }
 }
