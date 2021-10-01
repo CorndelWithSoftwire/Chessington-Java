f6194ea6633b9ba1fd06f23c9ce35b81f1f70629
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-01 12:27:33 +0100

    added test for knight cannot move off board left or right

--- src/test/java/training/chessington/model/pieces/KnightTest.java
+++ src/test/java/training/chessington/model/pieces/KnightTest.java
@@ -54,4 +54,40 @@ public class KnightTest {
         // Assert
         assertThat(moves).doesNotContain(new Move(coords, allyPawnCoords));
     }
+
+    @Test
+    public void knightCannotMoveOffBoardLeft() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(5, 0);
+        board.placePiece(coords, knight);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, -2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, -2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-2, -1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(2, -1)));
+    }
+
+    @Test
+    public void knightCannotMoveOffBoardRight() {
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(5, 7);
+        board.placePiece(coords, knight);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, 2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, 2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-2, 1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(2, 1)));
+    }
 }
