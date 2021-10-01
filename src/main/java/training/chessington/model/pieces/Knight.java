1935dacc1fbc6ed6b15ea68b507f09d60309919a
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-01 11:31:12 +0100

    knights can move in L shape manner

--- src/main/java/training/chessington/model/pieces/Knight.java
+++ src/main/java/training/chessington/model/pieces/Knight.java
@@ -6,6 +6,7 @@ import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Knight extends AbstractPiece {
@@ -15,6 +16,40 @@ public class Knight extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowableMoves = new ArrayList<>();
+
+        // laying rectangle
+        getCornersOfRectangle(from, board, allowableMoves, 1, 2);
+        // standing rectangle
+        getCornersOfRectangle(from, board, allowableMoves, 2, 1);
+
+        return allowableMoves;
+    }
+
+    private void getCornersOfRectangle(Coordinates from, Board board,List<Move> initAllowableMoves, int rowInc, int colInc) {
+        int fromRow = from.getRow();
+        int fromCol = from.getCol();
+
+        int[] rowArray = {fromRow - rowInc, fromRow + rowInc};
+        int[] colArray = {fromCol - colInc, fromCol + colInc};
+
+        for(int r: rowArray) {
+
+            // quick exit to next item
+            if (r < 0 | r > 7) {
+                continue;
+            }
+
+            for (int c: colArray) {
+                // quick exit to next item
+                if (c < 0 | c > 7) {
+                    continue;
+                }
+
+                Coordinates to = new Coordinates(r, c);
+                initAllowableMoves.add(new Move(from, to));
+
+            }
+        }
     }
 }
--- src/test/java/training/chessington/model/pieces/KnightTest.java
+++ src/test/java/training/chessington/model/pieces/KnightTest.java
@@ -1,7 +1,38 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class KnightTest {
+
+    @Test
+    public void knightCanMoveInLShapeMannerTwoByOne() {
+
+        // Arrange
+        Board board = Board.empty();
+        Piece knight = new Knight(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(4, 4);
+        board.placePiece(coords, knight);
+
+        // Act
+        List<Move> moves = knight.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2, 1)));
+    }
 }
