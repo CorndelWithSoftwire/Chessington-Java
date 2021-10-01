49212b274fe83886e8045559bbb054cbce936544
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-01 15:18:30 +0100

    bishop and move diagonally

--- src/main/java/training/chessington/model/pieces/Bishop.java
+++ src/main/java/training/chessington/model/pieces/Bishop.java
@@ -15,6 +15,36 @@ public class Bishop extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowableMoves = new ArrayList<>();
+
+        addTheDiagonal(from, board, allowableMoves);
+
+        return allowableMoves;
+    }
+
+    private void addTheDiagonal(Coordinates from, Board board, List<Move> initAllowableMoves) {
+        int[] incrementArray = {-1, 1};
+        int fromRow = from.getRow();
+        int fromCol = from.getCol();
+
+        for (int i: incrementArray) {
+            for (int j: incrementArray) {
+                int row = fromRow + i;
+                int col = fromCol + j;
+                while (row >= 0 && row <= 7 && col >= 0 && col <= 7) {
+                    Coordinates to = new Coordinates(row, col);
+                    Piece targetPiece = board.get(to);
+
+                    if (targetPiece != null) {
+                        if (targetPiece.getColour() != this.colour) { initAllowableMoves.add(new Move(from, to)); }
+                        break;
+                    }
+
+                    initAllowableMoves.add(new Move(from, to));
+                    row += i;
+                    col += j;
+                }
+            }
+        }
     }
 }
--- src/test/java/training/chessington/model/pieces/BishopTest.java
+++ src/test/java/training/chessington/model/pieces/BishopTest.java
@@ -1,7 +1,34 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class BishopTest {
+    @Test
+    public void bishopCanMoveDiagonally() {
+
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(4, 4);
+        board.placePiece(coords, bishop);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, -1)));
+    }
+
 }
